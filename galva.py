be1485e1b10a937b87cd7efaec412aa53e2187a3
Archy-git <artursuija@gmail.com>
2021-01-06 11:17:31 +0200

    Testins
    Izmainiitais

--- galva.py
+++ galva.py
@@ -14,9 +14,18 @@ def root():
     bb = float(request.args.get('b',default='0.',type=str))
     cc = float(request.args.get('c',default='0.',type=str))
     
-    rez = "Perimetrs=" + funcy.tr_per(aa,bb,cc)
+    rez = "Perimetrs=" + funci.tr_per(aa,bb,cc)
     return render_template("sveikaPasaule.html",vards="Trīsstūra parametri",rezultats=rez)
 
+@app.route('/pogasall',methods=['GET'])
+def pogasall():
+    aa = float(request.args.get('a',default='0.',type=str))
+    aa= "Ievadītā vērtība"
+    return render_template("sveikaPasaule.html",vards="Kontroļu pārbaude",rezultats=rez)
+
+@app.route('/pogas')
+def pogas():
+    return render_template("pogas.html",vards="Podziņas")
 
 @app.route('/tests')
 def health():
